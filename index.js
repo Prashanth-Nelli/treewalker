5cffa71453d097a6a007ee433c652289a1978b8a
Prashanth-Nelli <micprashanth@gmail.com>
2015-05-09 14:04:47 +0530

    formatted code

--- index.js
+++ index.js
@@ -1,73 +1,74 @@
 ;( function() {
 
-		function start(parent, childkey, cb) {
+	function start(parent, childkey, cb) {
 
-			var node = parent;
-			var child = childkey;
+		var node = parent;
+		var child = childkey;
 
-			for (var i = 0; i < node[child].length; i++) {
-				cb(node);
-				traverse(node[child][i]);
+		for (var i = 0; i < node[child].length; i++) {
+			cb(node);
+			traverse(node[child][i]);
+		}
+
+		function traverse(childnode) {
+
+			if (childnode[child].length == 0) {
+				cb(childnode);
+				return;
 			}
 
-			function traverse(childnode) {
-
-				if (childnode[child].length == 0) {
-					cb(childnode);
-					return;
-				}
-
-				for (var j = 0; j < childnode[child].length; j++) {
-					cb(childnode);
-					traverse(childnode[child][j]);
-				}
-
+			for (var j = 0; j < childnode[child].length; j++) {
+				cb(childnode);
+				traverse(childnode[child][j]);
 			}
 
 		}
 
-		function resolveTree(parent, childkey, cb) {
-			var node = parent;
-			var child = childkey;
-			
-			out:
-			for (var i = 0; i < node[child].length; i++) {
-				if (cb(node,[node],0)) {
-					break out;
-				}
-				walkCompare(node[child][i],node[child],i);
+	}
+
+	function resolveTree(parent, childkey, cb) {
+	
+		var node = parent;
+		var child = childkey;
+		
+		out:
+		for (var i = 0; i < node[child].length; i++) {
+			if (cb(node,[node],0)) {
+				break out;
+			}
+			walkCompare(node[child][i],node[child],i);
+		}
+
+		function walkCompare(childnode,siblings,index) {
+
+			if (cb(childnode,siblings,index)) {
+				return;
 			}
 
-			function walkCompare(childnode,siblings,index) {
+			if (childnode[child].length == 0) {
+				cb(childnode,siblings,index)
+				return;
+			} 
 
-				if (cb(childnode,siblings,index)) {
-					return;
+			Loop:
+			for (var j = 0; j < childnode[child].length; j++) {
+				if (cb(childnode[child][j]),childnode[child],j) {
+					break Loop;
 				}
-
-				if (childnode[child].length == 0) {
-					cb(childnode,siblings,index)
-					return;
-				} 
-
-				Loop:
-				for (var j = 0; j < childnode[child].length; j++) {
-					if (cb(childnode[child][j]),childnode[child],j) {
-						break Loop;
-					}
-					walkCompare(childnode[child][j],childnode[child],j);
-				}
-
+				walkCompare(childnode[child][j],childnode[child],j);
 			}
 
 		}
 
-		if ( typeof exports !== "undefined" && typeof module !== "undefined" && module.exports) {
-			exports = module.exports = {};
-			exports.walkTree = start;
-			exports.resolveTree = resolveTree;
-		} else {
-			this.walkTree = start;
-			this.resolveTree = resolveTree;
-		}
+	}
 
-	}.call(this));
+	if ( typeof exports !== "undefined" && typeof module !== "undefined" && module.exports) {
+		exports = module.exports = {};
+		exports.walkTree = start;
+		exports.resolveTree = resolveTree;
+	} else {
+		this.walkTree = start;
+		this.resolveTree = resolveTree;
+	}
+
+}.call(this));
